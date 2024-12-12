8882c07c413b5887a073130f702292df58f443b2
Nols <108218108+Zenara12@users.noreply.github.com>
2024-12-13 01:16:04 +0800

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<div id="footer">
+    <div class="copyright">
+        &copy;2017 CICTE Minor Projects. All rights reserved
+    </div>
+    <div class="footer_links"> 
+        <input type="Submit" style="border-radius:8px; font-size:20px; width:100px; height:30px;" Value="   Save    "/>
+    </div>
+</div>  
